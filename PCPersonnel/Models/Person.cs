6d96a7165c942d357e11d9b4567ede78e13db6c4
sebug <sebastian.gfeller@gmail.com>
2020-04-14 14:53:35 +0200

    First model

---
+++ PCPersonnel/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace PCPersonnel.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
