c6642f5bc51470ebf241cea10962c5051f98c673
sebug <sebastian.gfeller@gmail.com>
2020-04-14 14:37:13 +0200

    Adding anti forgery token basics

--- PCPersonnel/Pages/Index.cshtml.cs
+++ PCPersonnel/Pages/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,23 @@ namespace PCPersonnel.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        IFormFile ExcelFile { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public async Task OnPostAsync()
+        {
+            if (this.ExcelFile == null)
+            {
+                return;
+            }
+            using (var ms = new MemoryStream())
+            {
+                await this.ExcelFile.CopyToAsync(ms);
+            }
         }
     }
 }
