857caf4d4c7c8269effd264da2c770fabd1419f4
sebug <sebastian.gfeller@gmail.com>
2020-04-15 15:42:58 +0200

    With stats per day working

--- PCPersonnel/Services/StatsService.cs
+++ PCPersonnel/Services/StatsService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using PCPersonnel.Models;
 using PCPersonnel.Repositories;
+using System.Linq;
 
 namespace PCPersonnel.Services
 {
@@ -17,6 +18,31 @@ namespace PCPersonnel.Services
         {
             var result = new StatsByDate();
             result.FormattedDate = date.ToString("dd.MM.yyyy");
+
+            var personsAndPresence = this._personRepository.GetAll()
+                .Select(p => new
+                {
+                    Person = p,
+                    Presence = p.Presences.FirstOrDefault(p => p.Date.Date == date.Date)
+                }).ToList();
+
+            var presents = personsAndPresence
+                .Where(pp => pp.Presence != null && pp.Presence.Called &&
+                pp.Presence.Presence != null &&
+                pp.Presence.Presence.Equals("P", StringComparison.InvariantCultureIgnoreCase));
+
+            var rests = personsAndPresence
+                .Where(pp => pp.Presence != null && pp.Presence.Called &&
+                pp.Presence.Presence != null &&
+                pp.Presence.Presence.IndexOf("R", StringComparison.InvariantCultureIgnoreCase) >= 0);
+
+            result.PresentCount = presents
+                .Count();
+            result.RestCount = rests.Count();
+
+            result.MissionCount = presents.GroupBy(pp => pp.Person.Mission)
+                .ToDictionary(g => g.Key, g => g.Count());
+
             return result;
         }
     }
