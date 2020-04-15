e876d9b241935ad41c8388bd63369e995db0d1b0
sebug <sebastian.gfeller@gmail.com>
2020-04-15 15:24:44 +0200

    Adding stats by date

---
+++ PCPersonnel/Models/StatsByDate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PCPersonnel.Models
+{
+    public class StatsByDate
+    {
+        public string FormattedDate { get; set; }
+        public int PresentCount { get; set; }
+        public int RestCount { get; set; }
+        public Dictionary<string, int> MissionCount { get; set; }
+    }
+}
