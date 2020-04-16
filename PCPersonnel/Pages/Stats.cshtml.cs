d1ddc9c94f3f612f19535f20f859e7b535fd2d99
sebug <sebastian.gfeller@gmail.com>
2020-04-16 10:06:33 +0200

    With stats

---
+++ PCPersonnel/Pages/Stats.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PCPersonnel.Models;
+using PCPersonnel.Services;
+
+namespace PCPersonnel.Pages
+{
+    public class StatsModel : PageModel
+    {
+        private readonly IStatsService _statsService;
+
+        public StatsModel(IStatsService statsService)
+        {
+            this._statsService = statsService;
+        }
+
+        public StatsByDate StatsByDate { get; set; }
+
+        public void OnGet(string date)
+        {
+            DateTime dt;
+            if (date != null && DateTime.TryParse(date, out dt))
+            {
+                this.StatsByDate = this._statsService.GetStatsByDate(dt);
+            }
+        }
+    }
+}
