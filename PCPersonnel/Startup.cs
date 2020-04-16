1a7ee7abd31a963817204af05846c7de9967337c
sebug <sebastian.gfeller@gmail.com>
2020-04-16 10:55:42 +0200

    Adding google authentication package

--- PCPersonnel/Startup.cs
+++ PCPersonnel/Startup.cs
@@ -33,6 +33,8 @@ namespace PCPersonnel
             services.AddSingleton<IExcelFileRepository, InMemoryExcelFileRepository>();
             services.AddSingleton<IPersonRepository, PersonRepository>();
             services.AddTransient<IStatsService, StatsService>();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
