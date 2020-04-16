80089aeb8a5eb82f0bf32175a10686c0406632f6
sebug <sebastian.gfeller@gmail.com>
2020-04-16 08:41:59 +0200

    Adding person display

---
+++ PCPersonnel/Pages/Person.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PCPersonnel.Models;
+using PCPersonnel.Repositories;
+
+namespace PCPersonnel.Pages
+{
+    public class PersonModel : PageModel
+    {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonModel(IPersonRepository personRepository)
+        {
+            this._personRepository = personRepository;
+        }
+
+        public Person Person { get; set; }
+
+        public void OnGet(string avsNumber)
+        {
+            this.Person = this._personRepository.GetByAVSNumber(avsNumber);
+
+        }
+    }
+}
