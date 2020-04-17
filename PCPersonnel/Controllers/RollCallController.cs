a7dba005a205c4874c787f128d3fdaec71d490e0
sebug <sebastian.gfeller@gmail.com>
2020-04-17 09:53:28 +0200

    Roll call basic controller

---
+++ PCPersonnel/Controllers/RollCallController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PCPersonnel.Models;
+using PCPersonnel.Services;
+
+namespace PCPersonnel.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RollCallController : ControllerBase
+    {
+        private readonly IRollCallService _rollCallService;
+
+        public RollCallController(IRollCallService rollCallService)
+        {
+            this._rollCallService = rollCallService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public RollCall Get(string id, string date = null)
+        {
+            DateTime dt;
+            if (date == null || !DateTime.TryParse(date, out dt))
+            {
+                dt = DateTime.Now.Date;
+            }
+            var result = this._rollCallService.GetByEntryAndDate(id, dt);
+            return result;
+        }
+    }
+}
