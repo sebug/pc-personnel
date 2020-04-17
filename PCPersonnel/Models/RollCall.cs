4abb00bee50ce57213160957bfa3e844d8c94209
sebug <sebastian.gfeller@gmail.com>
2020-04-17 09:43:35 +0200

    Adding roll call model

---
+++ PCPersonnel/Models/RollCall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PCPersonnel.Models
+{
+    public class RollCall
+    {
+        public DateTime Date { get; set; }
+        public string Entry { get; set; }
+        public List<RollCallPerson> Presences { get; set; }
+    }
+}
---
+++ PCPersonnel/Models/RollCallPerson.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PCPersonnel.Models
+{
+    public class RollCallPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Presence { get; set; }
+    }
+}
