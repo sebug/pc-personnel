4751661b22e0f4ad430a5b99f468290425b4fe7d
sebug <sebastian.gfeller@gmail.com>
2020-04-17 10:37:40 +0200

    Roll call table is there

---
+++ PCPersonnel/Pages/RollCall.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PCPersonnel.Models;
+using PCPersonnel.Services;
+
+namespace PCPersonnel.Pages
+{
+    public class RollCallModel : PageModel
+    {
+        private readonly IRollCallService _rollCallService;
+        public RollCallModel(IRollCallService rollCallService)
+        {
+            this._rollCallService = rollCallService;
+        }
+
+        public RollCall RollCall { get; set; }
+
+        public void OnGet(string date, string placeOfEntry)
+        {
+            DateTime dt;
+            if (date == null ||
+                !DateTime.TryParse(date, out dt))
+            {
+                dt = DateTime.Now.Date;
+                this.RollCall = this._rollCallService.GetByEntryAndDate(placeOfEntry, dt);
+            }
+            else
+            {
+                this.RollCall = this._rollCallService.GetByEntryAndDate(placeOfEntry, dt.Date);
+            }
+        }
+    }
+}
