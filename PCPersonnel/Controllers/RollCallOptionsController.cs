ba7a331e17bd220f3c1507d978954621b5e87fd4
sebug <sebastian.gfeller@gmail.com>
2020-04-17 10:18:07 +0200

    Adding roll call options

---
+++ PCPersonnel/Controllers/RollCallOptionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PCPersonnel.Models;
+using PCPersonnel.Services;
+
+namespace PCPersonnel.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RollCallOptionsController : ControllerBase
+    {
+        private readonly IRollCallService _rollCallService;
+        public RollCallOptionsController(IRollCallService rollCallService)
+        {
+            this._rollCallService = rollCallService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public RollCallOptions Get(string id)
+        {
+            DateTime dt;
+            if (id == null || !DateTime.TryParse(id, out dt))
+            {
+                return null;
+            }
+            var options = this._rollCallService.GetRollCallOptions(dt.Date);
+            return options;
+        }
+    }
+}
